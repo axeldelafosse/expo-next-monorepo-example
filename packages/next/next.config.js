ab40bd7a61b7a74410ceda3a98be934a8489383d
Axel Delafosse <axeldelafosse@gmail.com>
2021-09-07 17:55:21 +0200

    add next bundle analyzer plugin

--- packages/next/next.config.js
+++ packages/next/next.config.js
@@ -1,5 +1,8 @@
 const { withExpo } = require('@expo/next-adapter');
 const withPlugins = require('next-compose-plugins');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+});
 const withTM = require('next-transpile-modules')([
   'app',
   '@gorhom/bottom-sheet',
@@ -11,6 +14,10 @@ const withTM = require('next-transpile-modules')([
 const nextConfig = {};
 
 module.exports = withPlugins(
-  [withTM, [withExpo, { projectRoot: __dirname + '/../..' }]],
+  [
+    withTM,
+    withBundleAnalyzer,
+    [withExpo, { projectRoot: __dirname + '/../..' }]
+  ],
   nextConfig
 );
