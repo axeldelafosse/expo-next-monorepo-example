c32b6e984268c47ead4cf3e1b283bf365eae917b
Axel Delafosse <axeldelafosse@gmail.com>
2021-10-29 11:47:02 +0200

    clean

--- packages/app/navigation/next-tab-navigator.tsx
+++ packages/app/navigation/next-tab-navigator.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 import { TabBarIcon } from 'app/navigation/tab-bar-icon'
 
