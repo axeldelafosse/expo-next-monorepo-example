249029fd7f077146a18e3276530afd3a98ee5fd8
Fernando Rojo <frojo@sas.upenn.edu>
2021-11-02 13:27:55 -0400

    consolidate types

--- packages/app/navigation/bottom-tab-navigator-old/types.ts
+++ packages/app/navigation/bottom-tab-navigator-old/types.ts
@@ -1,21 +1,4 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { NavigatorScreenParams } from '@react-navigation/native'
-import {
-  HomeStackParams,
-  PlaylistsStackParams,
-  ProfileStackParams
-} from '../types'
-
-export type BottomTabNavigatorParams = {
-  homeTab: NavigatorScreenParams<HomeStackParams>
-  playlistsTab: NavigatorScreenParams<PlaylistsStackParams>
-  profileTab: NavigatorScreenParams<ProfileStackParams>
-}
-
-declare global {
-  namespace ReactNavigation {
-    interface RootParamList extends BottomTabNavigatorParams {}
-  }
-}
+import { BottomTabNavigatorParams } from '../types'
 
 export const BottomTab = createBottomTabNavigator<BottomTabNavigatorParams>()
